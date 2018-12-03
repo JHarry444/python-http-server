98cfc0616dd0f20685a4f25682d1aafff17a499f
JHarry444 <jordan.harrison@academytrainee.com>
2018-12-03 13:40:05 +0000

    Converted to python 2, added file input.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import http.server
+import SimpleHTTPServer
 import socketserver
 import os
 
@@ -8,7 +8,7 @@ PORT = 9000
 web_dir = os.path.join(os.path.dirname(__file__), 'static')
 os.chdir(web_dir)
 
-Handler = http.server.SimpleHTTPRequestHandler
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 httpd = socketserver.TCPServer(("", PORT), Handler)
 print("serving at port", PORT)
 httpd.serve_forever()
