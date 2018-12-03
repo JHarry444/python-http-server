2a5cf6bf456fc26162c4f1026654765d73d33ac6
JHarry444 <jordan.harrison@academytrainee.com>
2018-12-03 16:45:22 +0000

    Initial attempt at python rest api.

--- app.py
+++ app.py
@@ -2,7 +2,11 @@
 import SimpleHTTPServer
 import SocketServer
 import os
+from flask import Flask
+from flask_restful import Api, Reource, reqparse
 
+app = Flask(__name__)
+api = Api(app)
 PORT = 9000
 
 web_dir = os.path.join(os.path.dirname(__file__), 'static')
